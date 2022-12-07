c6e111953985184add56fc0fa70557355e346b5c
Henrique Barbosa <henrikleod@gmail.com>
2022-12-06 21:32:06 -0300

    feat: folder indexes

--- index.ts
+++ index.ts
@@ -1,1 +1,37 @@
-// silence is golden
+/* TYPES > API RESPONSES */
+
+import {
+    CanDonate,
+    CardFees,
+    DonationsCount,
+    NonProfitImpact,
+    PoolBalance,
+    PromoterCardGiving,
+    PromoterDonation,
+} from './types/apiResponses';
+
+/* TYPES > ENTITIES */
+
+import {
+    Cause,
+    CreditCardPayment,
+    CurrentNetwork,
+    Donation,
+    Impact,
+    Integration,
+    IntegrationTask,
+    NonProfit,
+    Notification,
+    Offer,
+    Pool,
+    Story,
+    User,
+} from './types/entities';
+
+/* TYPES > ENUMS */
+
+import {
+    TransactionStatus,
+    Currencies,
+    Languages,
+} from './types/enums';
---
+++ types/apiResponses/index.ts
@@ -0,0 +1,17 @@
+import CanDonate from './CanDonate';
+import CardFees from './CardFees';
+import DonationsCount from './DonationsCount';
+import NonProfitImpact from './NonProfitImpact';
+import PoolBalance from './PoolBalance';
+import PromoterCardGiving from './PromoterCardGiving';
+import PromoterDonation from './PromoterDonation';
+
+export {
+    CanDonate,
+    CardFees,
+    DonationsCount,
+    NonProfitImpact,
+    PoolBalance,
+    PromoterCardGiving,
+    PromoterDonation,
+}
