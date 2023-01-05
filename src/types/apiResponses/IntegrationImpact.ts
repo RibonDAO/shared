4d42758df5c82c007d1fa05013a4139ddb6df231
Nicholas Marques <devnicknish@gmail.com>
2023-01-05 15:49:50 -0300

    feat: add absolute to relative path when building

--- src/types/apiResponses/IntegrationImpact.ts
+++ src/types/apiResponses/IntegrationImpact.ts
@@ -3,6 +3,8 @@ import { NonProfit } from "../entities";
 export default interface IntegrationImpact {
   totalDonations: number;
   totalDonors: number;
+  totalDonorsTrend: number;
+  totalDonationsTrend: number;
   impactPerNonProfit: [{
     nonProfit: NonProfit;
     impact: number;
