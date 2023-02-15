d2ef49bf2495ebff4fee2188a9aa04723e127c96
Nicholas Marques <devnicknish@gmail.com>
2023-02-15 13:21:47 -0300

    feat: add PersonPayment

---
+++ src/types/entities/PersonPayment.ts
@@ -0,0 +1,27 @@
+import Offer from "./Offer";
+import Cause from "./Cause";
+import NonProfit from "./NonProfit";
+
+export default interface PersonPayment {
+  id: string;
+  amountCents: number;
+  cryptoAmount: number;
+  offer: Offer;
+  page: number;
+  paidDate: string;
+  payment_method: string;
+  person: {
+    id: string;
+    customer: {
+      email: string;
+    };
+    guest: {
+      wallet_address: string;
+    };
+  };
+  serviceFees: number;
+  receiver: NonProfit | Cause;
+  status: string;
+  totalItems: number;
+  totalPages: number;
+}
