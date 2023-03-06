c04187b961032a77cc8ee30757563a484ba0d069
Henrique Barbosa <henrikleod@gmail.com>
2023-03-06 12:18:05 -0300

    feat: article and author entities

---
+++ src/types/entities/Article.ts
@@ -0,0 +1,13 @@
+import Author from "./Author";
+
+export default interface Article {
+  id: number;
+  title: string;
+  imageUrl: string;
+  visible: boolean;
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+  link: string;
+  author: Author;
+}
