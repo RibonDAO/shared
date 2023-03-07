3ffc9292c12779d3085c53368476c09c87400a1a
Henrique Barbosa <henrikleod@gmail.com>
2023-03-07 13:02:30 -0300

    feat: add sortable params

--- src/services/api/articlesApi/index.ts
+++ src/services/api/articlesApi/index.ts
@@ -5,16 +5,22 @@ import { apiGetWithParams, apiGet } from "..";
 type ArticlesList = {
   perPage?: number;
   page?: number;
+  sort?: string;
+  sortDir?: string;
 };
 
 const articlesApi = {
   getArticlesList: ({
     perPage = 10,
     page = 1,
+    sort = "published_at",
+    sortDir = "desc",
   }: ArticlesList): Promise<AxiosResponse<Article[]>> =>
     apiGetWithParams("news/articles", {
       per: perPage,
       page,
+      sort,
+      sort_dir: sortDir,
     }),
   getArticle: (id: any): Promise<AxiosResponse<Article>> =>
     apiGet(`news/articles/${id}`),
