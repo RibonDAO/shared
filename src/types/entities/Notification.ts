641abf03d9c755718a5774be7c582def5dd70316
letbueno <leticia.bueno.martins@gmail.com>
2023-04-13 14:43:35 -0300

    feat: add more params on notifications type

--- src/types/entities/Notification.ts
+++ src/types/entities/Notification.ts
@@ -1,7 +1,19 @@
 export default interface Notification {
-  id: number;
+  id?: number;
   message: string;
-  linkMessage: string;
-  link: string;
-  color: string;
+  type: "success" | "error" | "warning" | "info";
+  link?: string;
+  timeout?: number;
+  linkMessage?: string;
+  icon?: string;
+  backgroundColor?: string;
+  textColor?: string;
+  position?:
+    | "top-right"
+    | "top-left"
+    | "bottom-right"
+    | "bottom-left"
+    | "top-center"
+    | "bottom-center";
+  onClose?: () => void;
 }
