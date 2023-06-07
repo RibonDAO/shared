a4eca026fe377090bccd4238c1b6a2fd85411af3
letbueno <leticia.bueno.martins@gmail.com>
2023-06-07 14:18:26 -0300

    fix: var

--- src/lib/masks/maskForTaxId/index.ts
+++ src/lib/masks/maskForTaxId/index.ts
@@ -2,9 +2,9 @@ import { Languages } from "types/enums/Languages";
 import { TaxIdPTBR } from "types/enums/TaxIdPTBR";
 import { TaxIdUS } from "types/enums/TaxIdUS";
 
-export function maskForTaxId(coin: string, currentLang: Languages) {
+export function maskForTaxId(country: string, currentLang: Languages) {
   const TaxId = currentLang === Languages.PT ? TaxIdPTBR : TaxIdUS;
-  const key = coin
+  const key = country
     .toUpperCase()
     .replace(/\s/g, "")
     .split("(")[0] as keyof typeof TaxId;
