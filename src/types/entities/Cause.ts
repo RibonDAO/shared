f1b9b2b84ee7fc67100b142d659c2b2ac8f9401f
letbueno <leticia.bueno.martins@gmail.com>
2023-07-27 11:08:36 -0300

    feat: add param of type

--- src/types/entities/Cause.ts
+++ src/types/entities/Cause.ts
@@ -13,5 +13,5 @@ export default interface Cause {
   defaultPool?: string;
   coverImageDescription?: string;
   mainImageDescription?: string;
-  hasPoolBalance?: boolean;
+  poolBalance?: boolean;
 }
