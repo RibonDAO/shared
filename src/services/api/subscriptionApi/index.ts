04cda5f9e9de55939c507bc22244d0cd9927a790
Karina Almeida <s.karinaalmeida@gmail.com>
2023-08-24 17:45:46 -0300

    feat: removed post and fix routes

--- src/services/api/subscriptionApi/index.ts
+++ src/services/api/subscriptionApi/index.ts
@@ -1,18 +1,13 @@
 import { AxiosResponse } from "axios";
 
-import { apiGet, apiPost, apiPut } from "..";
+import { apiGet, apiPut } from "..";
 
 const subscriptionApi = {
   getSubscription: (id: string | number): Promise<AxiosResponse<any>> =>
-    apiGet(`payments/subscription/${id}`),
-
-  postSendCancelSubscriptionEmail: (
-    id: string | number,
-  ): Promise<AxiosResponse<{}>> =>
-    apiPost("payments/send_cancel_subscription_email", { id }),
+    apiGet(`users/subscription/${id}`),
 
   putCancelSubscription: (id: string | number): Promise<AxiosResponse<any>> =>
-    apiPut(`payments/cancel_subscription/${id}`, {}),
+    apiPut(`subscriptions/cancel_subscription/${id}`, {}),
 };
 
 export default subscriptionApi;
