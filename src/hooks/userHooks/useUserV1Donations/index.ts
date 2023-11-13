523f8a8e0856510be0056abaa1911da164ada4c9
Thais Correa <thais@ribon.io>
2023-11-13 15:19:14 -0300

    feat: users v1 can donate

--- src/hooks/userHooks/useUserV1Donations/index.ts
+++ src/hooks/userHooks/useUserV1Donations/index.ts
@@ -1,4 +1,6 @@
-import { userDonationApi } from "services/index";
+import { userDonationApi, emptyRequest } from "services/index";
+import { CanDonate } from "types/apiResponses";
+import { useApi } from "hooks/useApi";
 
 function useUserV1Donations() {
   async function donate(
@@ -34,8 +36,48 @@ function useUserV1Donations() {
     );
   }
 
+  function checkCanDonate(
+    integrationId: number | string | null,
+    platform: "app" | "web",
+    voucherId?: string,
+  ) {
+    const { data, isLoading, refetch } = useApi<CanDonate>({
+      key: "canDonate",
+      fetchMethod: () => {
+        if (integrationId)
+          return userDonationApi.postCanDonate(integrationId, platform, voucherId);
+
+        return emptyRequest();
+      },
+      options: {
+        enabled: !!integrationId,
+      },
+      criteria: [integrationId, voucherId],
+    });
+
+    function formattedCanDonate() {
+      if (data === undefined) return true;
+
+      return data.canDonate;
+    }
+
+    function formattedDonateApp() {
+      if (data === undefined) return true;
+
+      return data.donateApp;
+    }
+
+    return {
+      canDonate: formattedCanDonate(),
+      donateApp: formattedDonateApp(),
+      isLoading,
+      refetch,
+    };
+  }
+
   return {
     donate,
+    checkCanDonate,
   };
 }
 
