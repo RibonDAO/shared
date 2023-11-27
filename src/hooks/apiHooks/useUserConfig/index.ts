018650eac5abb165309f35da6616cf0e7645c25b
Nathiely Macedo <nathielymm@gmail.com>
2023-11-27 17:26:20 -0300

    feat: adds getUserConfig to the useUserConfig hook

--- src/hooks/apiHooks/useUserConfig/index.ts
+++ src/hooks/apiHooks/useUserConfig/index.ts
@@ -1,12 +1,28 @@
 import { UserConfig } from "types/entities";
 import userConfigApi from "services/api/userConfigApi";
+import { useApi } from "hooks/useApi";
 
 function useUserConfig() {
+  function userConfig() {
+    const {
+      refetch,
+      isLoading,
+      data: config,
+    } = useApi<UserConfig>({
+      key: "UserConfig",
+      fetchMethod: () => userConfigApi.getUserConfig(),
+    });
+
+    return { config, refetch, isLoading };
+  }
+
   async function updateUserConfig(userId: number, data: UserConfig) {
-    await userConfigApi.postUpdateUserConfig(userId, data);
+    const { data: config } = await userConfigApi.postUpdateUserConfig(userId, data);
+    return config;
   }
 
   return {
+    userConfig,
     updateUserConfig,
   };
 }
--- src/services/api/userConfigApi/index.ts
+++ src/services/api/userConfigApi/index.ts
@@ -1,12 +1,14 @@
 import { AxiosResponse } from "axios";
 import UserConfig from "types/entities/UserConfig";
-import { apiPost } from "..";
+import { apiGet, apiPost } from "..";
 
 const userConfigApi = {
   postUpdateUserConfig: (
     userId: number,
     data: UserConfig,
   ): Promise<AxiosResponse<any>> => apiPost(`users/${userId}/configs`, data),
+
+  getUserConfig: (): Promise<AxiosResponse<any>> => apiGet("users/configs"),
 };
 
 export default userConfigApi;
