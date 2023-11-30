549ddbdb03d50d5d73ccbd2e31ede17d012e8d17
mendesmcmg <claraplanejamento@gmail.com>
2023-11-30 14:54:49 -0300

    fix: fixes parameter of postValidateExtraTicket

--- src/services/user/userAccountApi/index.ts
+++ src/services/user/userAccountApi/index.ts
@@ -4,8 +4,8 @@ import { apiPost } from "..";
 const userAccountApi = {
   postSendValidatedEmail: (): Promise<AxiosResponse<any>> =>
     apiPost("account/send_validated_email", {}),
-  postValidateExtraTicket: (email?: string): Promise<AxiosResponse<any>> =>
-    apiPost("account/send_validated_email", { email }),
+  postValidateExtraTicket: (token?: string): Promise<AxiosResponse<any>> =>
+    apiPost("account/send_validated_email", { token }),
 };
 
 export default userAccountApi;
