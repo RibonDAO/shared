75809d379e7812b9b03c4bc1981a000751559810
letbueno <leticia.bueno.martins@gmail.com>
2024-02-01 17:02:57 -0300

    feat: add collect and donate by integration

--- src/hooks/apiHooks/useTickets/index.ts
+++ src/hooks/apiHooks/useTickets/index.ts
@@ -29,9 +29,32 @@ function useTickets() {
     return data;
   }
 
+  async function collectAndDonateByIntegration(
+    integrationId: string | number,
+    nonProfitId: number,
+    email: string,
+    platform: string,
+    utmSource?: string,
+    utmMedium?: string,
+    utmCampaign?: string,
+  ) {
+    const { data } = await ticketsApi.collectAndDonateByIntegration(
+      integrationId,
+      nonProfitId,
+      email,
+      platform,
+      utmSource,
+      utmMedium,
+      utmCampaign,
+    );
+
+    return data;
+  }
+
   return {
     canCollectByIntegration,
     collectByIntegration,
+    collectAndDonateByIntegration,
   };
 }
 export default useTickets;
