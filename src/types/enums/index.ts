03c67dd6a849d46d5171332258df5c829da1332d
Nathiely Macedo <nathielymm@gmail.com>
2024-02-08 17:07:44 -0300

    fix: export categories

--- src/types/enums/index.ts
+++ src/types/enums/index.ts
@@ -2,5 +2,6 @@ import { Currencies } from "./Currencies";
 import { Languages } from "./Languages";
 import { TransactionStatus } from "./TransactionStatus";
 import { Gateways } from "./Gateways";
+import { Categories } from "./Categories";
 
-export { Currencies, Languages, TransactionStatus, Gateways };
+export { Currencies, Categories, Languages, TransactionStatus, Gateways };
