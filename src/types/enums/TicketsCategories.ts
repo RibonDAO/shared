5b28d4d01337b374b8e5243dc22eed437548c71b
letbueno <leticia.bueno.martins@gmail.com>
2024-03-12 11:51:37 -0300

    feat: add typo

---
+++ src/types/enums/TicketsCategories.ts
@@ -0,0 +1,7 @@
+// eslint-disable-next-line no-shadow
+export enum Sources {
+  // eslint-disable-next-line no-unused-vars
+  MONTHLY = "monthly",
+  // eslint-disable-next-line no-unused-vars
+  DAILY = "daily",
+}
