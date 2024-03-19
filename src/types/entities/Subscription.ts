b037caeeaf6e21baf191f13f442e87264397bc0b
Nathiely Macedo <nathielymm@gmail.com>
2024-03-19 14:50:27 -0300

    feat: adds personPayments to Subscription

--- src/types/entities/Subscription.ts
+++ src/types/entities/Subscription.ts
@@ -1,6 +1,7 @@
 import Cause from "./Cause";
 import NonProfit from "./NonProfit";
 import Offer from "./Offer";
+import PersonPayment from "./PersonPayment";
 
 export default interface Subscription {
   id: string | number;
@@ -10,4 +11,5 @@ export default interface Subscription {
   nextPaymentAttempt: string;
   offer: Offer;
   receiver: NonProfit | Cause;
+  personPayments: PersonPayment[];
 }
