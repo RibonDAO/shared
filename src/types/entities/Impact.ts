3f9dea2259fc4952957a39615fc48c29d395d832
Pedro Santos <pedrosantosdevelop@gmail.com>
2024-06-24 23:50:09 -0300

    feat: adds donation count to impact

--- src/types/entities/Impact.ts
+++ src/types/entities/Impact.ts
@@ -3,6 +3,7 @@ import NonProfit from "./NonProfit";
 export default interface Impact {
   id: number;
   impact: string;
+  donationCount: string;
   walletAddress: string;
   totalDonated: number;
   nonProfit: NonProfit;
