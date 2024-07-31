404ddf7f7cf59be4afb960e8b5087ca9a8f4d02c
mendesmcmg <claraplanejamento@gmail.com>
2024-07-31 16:18:09 -0300

    fix: adds company and subscription to user entity

--- src/types/entities/User.ts
+++ src/types/entities/User.ts
@@ -1,7 +1,12 @@
+import Integration from "./Integration";
+import Subscription from "./Subscription";
+
 export default interface User {
   id: number;
   email: string;
   lastDonationAt?: string;
   lastDonatedCause?: number;
   donateNative?: boolean;
+  company?: Integration;
+  directTransferSubscription?: Subscription
 }
